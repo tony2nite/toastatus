3fdc995856e7c86aff0bfdd0b95374d04e51f63e
Anthony Hughes <tony2nite@anthony-pro.local>
2010-12-06 11:03:32 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Toastatus::Application.config.secret_token = 'd9de1f49f68dd08e791063d849e3cd68c81f924a3c7ab402ccf6442996184704bb1b88a0e0d0449436717d2610202c29ea2055b997e6bdb83a2ac31dca84db40'
